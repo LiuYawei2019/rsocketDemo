9aa0702275b161aee939fe03d67442ce7f0625fb
LiuYawei2019 <52200681+LiuYawei2019@users.noreply.github.com>
2025-03-06 20:53:30 +0800

    Update RSocketServer7082.java

--- src/main/java/RSocketServer7082.java
+++ src/main/java/RSocketServer7082.java
@@ -18,7 +18,7 @@ public class RSocketServer7082 {
                                 return Mono.just(DefaultPayload.create("Server 3 response"))
                                         .delayElement(Duration.ofMillis(100));
                             }))
-                    .bindNow(TcpServerTransport.create(7082));
+                    .bindNow(TcpServerTransport.create("127.0.0.1", 7082));
             while(true) {
                 try {
                     TimeUnit.MILLISECONDS.sleep(1000000);
