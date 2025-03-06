45e374b247440b32483993d10b0ba0a63e38cbe0
LiuYawei2019 <52200681+LiuYawei2019@users.noreply.github.com>
2025-03-06 20:25:18 +0800

    Create RSocketServer7080.java

---
+++ src/main/java/RSocketServer7080.java
@@ -0,0 +1,33 @@
+import io.rsocket.SocketAcceptor;
+import io.rsocket.core.RSocketServer;
+import io.rsocket.transport.netty.server.TcpServerTransport;
+import io.rsocket.util.DefaultPayload;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class RSocketServer7080 {
+
+    public static void main(String[] args) {
+        try {
+            RSocketServer.create(
+                            SocketAcceptor.forRequestResponse(
+                                    p -> {
+                                        System.out.println("Server 1 got fnf " + p.getDataUtf8());
+                                        return Mono.just(DefaultPayload.create("Server 1 response"))
+                                                .delayElement(Duration.ofMillis(100));
+                                    }))
+                    .bindNow(TcpServerTransport.create(7080));
+            while(true) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10000);
+                } catch (Exception ignore) {
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
